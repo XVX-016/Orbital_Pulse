b8a04f026813fb921b965daa5078a08c49786fa3
XVX-016 <tanmmay2005@gmail.com>
2026-08-22 19:34:41 +0530

    feat: add SatQuery AI Analyze.tsx workspace, remove legacy ChangeDetection.tsx & route

--- client/App.tsx
+++ client/App.tsx
@@ -8,7 +8,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "@/components/layout/Layout";
 import Index from "./pages/Index";
-import ChangeDetection from "./pages/ChangeDetection";
+import Analyze from "./pages/Analyze";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 
@@ -23,7 +23,7 @@ const App = () => (
         <Layout>
           <Routes>
             <Route path="/" element={<Index />} />
-            <Route path="/change-detection" element={<ChangeDetection />} />
+            <Route path="/analyze" element={<Analyze />} />
             <Route path="/about" element={<About />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
